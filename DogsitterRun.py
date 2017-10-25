e5b4c3f7d1a6f714080396d318432b06f5da16d3
Joel Goodman <goodmajo@oregonstate.edu>
2017-10-25 13:23:25 -0700

    Commit

---
+++ DogsitterRun.py
@@ -0,0 +1,286 @@
+import RPi.GPIO as GPIO
+import pygame
+import datetime
+import time
+import smtplib
+from git import Repo
+
+class Box(object):
+    def __init__(self):
+        print("Box is on")
+
+    class Trigger_LED(object):
+        def __init__(self, green_pin, blue_pin):
+            print("We have a trigger LED")
+            self.state = "Off"
+            self.green_pin = green_pin
+            self.blue_pin = blue_pin
+
+        def update(self, message, sender):
+            print(message)
+            print("Trigger LED received update from", sender)
+            if sender == "upstairs":
+                print("Signal recieved from", sender)
+                for i in range(1, 3):
+                    GPIO.output(self.green_pin, GPIO.HIGH)
+                    time.sleep(1.0)
+                    GPIO.output(self.green_pin, GPIO.LOW)
+                    time.sleep(1.0)
+            if sender == "downstairs":
+                print("Signal recieved from", sender)
+                for i in range(1, 3):
+                    GPIO.output(self.blue_pin, GPIO.HIGH)
+                    time.sleep(1.0)
+                    GPIO.output(self.blue_pin, GPIO.LOW)
+                    time.sleep(1.0)
+
+    class Power_LED(object):
+        def __init__(self, pin):
+            print("We have a power LED")
+            self.state = "On"
+            self.pin = pin
+            GPIO.output(self.pin, GPIO.HIGH)
+
+
+class Dog(object):
+    def __init__(self):
+        print("Creating a dog")
+
+        self.location_in_house = "downstairs"
+        self.trips_through_house = 0
+
+
+class Lights(object):
+    def __init__(self):
+        print("Lights object exists now")
+        self.state = "Off"
+        self.on_location = None
+        self.upstairs_light = "Off"
+        self.downstairs_light = "Off"
+
+    def update(self, message, sender):
+        print(message)
+        print("Lights received update from", sender)
+        if message == "On":
+            if sender == "upstairs light":
+                self.upstairs_light = "On"
+                self.on_location = "upstairs"
+            else:
+                self.downstairs_light = "On"
+                self.on_location = "downstairs"
+        if message == "Off":
+            if sender == "upstairs light":
+                self.upstairs_light = "Off"
+            else:
+                self.downstairs_light = "Off"
+        if self.downstairs_light == "On" or self.upstairs_light == "On":
+            self.state = "On"
+        if self.downstairs_light == "Off" and self.upstairs_light == "Off":
+            self.state = "Off"
+            self.on_location = None
+
+
+class Light(object):
+    def __init__(self, name):
+        print("Making light object called", name)
+        self.name = name
+        self.state = "Off"
+        self.observers = set()
+
+    def register(self, relay):
+        self.observers.add(relay)
+
+    def unregister(self, relay):
+        self.observers.discard(relay)
+
+    def dispatch(self, message, sender):
+        for observer in self.observers:
+            observer.update(message, sender)
+
+    def update(self, message, sender):
+        if sender == self.name:
+            if message == "Off":
+                self.state = "Off"
+            if message == "On":
+                self.state = "On"
+        else:
+            if message == "Off":
+                self.state = "On"
+            if message == "On":
+                self.state = "Off"
+
+
+class Relay(object):
+    def __init__(self, name, pin):
+        self.name = name
+        self.pin = pin
+        print("Relay initialized with name", self.name)
+
+    def update(self, message, sender):
+        print("relay received update from", sender)
+        if message == "Off":
+            print("I just turned the light off")
+            GPIO.output(self.pin, GPIO.LOW)
+        if message == "On":
+            print("I just turned the light on")
+            GPIO.output(self.pin, GPIO.HIGH)
+
+
+class PIR_Sensor(object):
+    def __init__(self, name="PIR"):
+        self.name = name
+        self.observers = set()
+
+    def register(self, led):
+        self.observers.add(led)
+
+    def unregister(self, led):
+        self.observers.discard(led)
+
+    def dispatch(self, message, sender):
+        for observer in self.observers:
+            observer.update(message, sender)
+
+
+class Initial_Location_Switch(object):
+    def __init__(self):
+        print("Checking state of Initial Position Switch")
+
+
+class Stereo(object):
+    def __init__(self, filename):
+        print("Making stereo object")
+        self.state = "Off"
+        self.filename = filename
+        self.timer = time.time()
+
+    def play_audio(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load(self.filename)
+        pygame.mixer.music.play()
+        self.timer = time.time()
+        self.state = "On"
+
+
+def print_time(a_string):
+    now = datetime.datetime.now()
+    print(now, ":")
+    print(a_string)
+
+def Html_Author(temperature, a_dog, the_lights, a_stereo):
+    # Make an html file with your variables nicely slipped into the body of the page.
+    f = open('index.html', 'w')
+
+    make_html = f"""
+    <html>
+    <head>
+      <title>Where is Olive Today?</title>
+      <style>
+      div.container {{
+          width: 100%;
+          border: 1px solid gray;
+      }}
+
+      header, footer {{
+          padding: 1em;
+          color: white;
+          background-color: black;
+          clear: left;
+          text-align: center;
+      }}
+
+      nav {{
+          float: left;
+          max-width: 200px;
+          margin: 0;
+          padding: 1em;
+      }}
+
+      nav ul {{
+          list-style-type: none;
+          padding: 0;
+      }}
+
+      nav ul a {{
+          text-decoration: none;
+      }}
+
+      article {{
+          margin-left: 400px;
+          border-left: 1px solid gray;
+          padding: 1em;
+          overflow: hidden;
+      }}
+      </style>
+      </head>
+      <body>
+
+    <div class="flex-container">
+    <header>
+      <h1>Where is Olive Today?</h1>
+      <h9>Spoiler: she's probably sleeping somewhere...</h9>
+    </header>
+
+    <nav class="nav">
+      <img src="OliveThePitBull.jpg" alt="Olive the Pit Bull" width="337.5" height="450">
+    </nav>
+
+    <article class="article">
+    <h1><font color=”#0000FF”>{off_message}</font></h1>
+      <h1>Olive is:</h1>
+        <p>{a_dog.location_in_house}</p>
+      <h1>Trips up and down the stairs:</h1>
+        <p>{a_dog.trips_through_house}</p>
+      <h1>Temperature:</h1>
+      <p>{temperature}° farenheit</p>
+
+      <h1>White Noise Status:</h1>
+      <p>{a_stereo.state}</p>
+
+      <h1>Lights are:</h1>
+      <p>{the_lights.state} {the_lights.on_location}</p>
+    </article>
+    </article>
+
+    <footer>
+    <a>Updated : {datetime.datetime.now()}</a>
+    </footer>
+    </div>
+    </body>
+    </html>
+    """
+
+    f.write(make_html)
+    f.close()
+
+    # Now do Git stuff to see the webpage update
+    repo_dir = ''
+    repo = Repo(repo_dir)
+    file_list = ['index.html']
+    commit_message = 'Updating html'
+    repo.index.add(file_list)
+    repo.index.commit(commit_message)
+    origin = repo.remote('origin')
+    origin.push()
+
+
+def Send_Mail(temperature, sound_level, the_stereo, the_lights):
+    print("Sending an email!")
+    mail_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    mail_server.ehlo()
+
+    email_sender = 'whereisolive.sender@gmail.com'
+    email_password = '0l1v3isadog'
+    email_receiver = 'whereisolivetoday.receiver@gmail.com'
+
+    email_subject = "Where is Olive - Temperature is {}.".format(temperature)
+    greetings = "Hello!\n"
+    temp_msg = "Temp is currently = {0} degrees F\n".format(temperature)
+    sound_msg = "Sound level is currently {0} dB. The stereo is {1}.\n".format(sound_level, the_stereo.state)
+    lights_msg = "The lights are {0} {1}.\n".format(the_lights.state, the_lights.on_location)
+    email_text = greetings + temp_msg + sound_msg + lights_msg
+    email_message = 'Subject: {}\n\n{}'.format(email_subject, email_text)
+
+    mail_server.login(email_sender, email_password)
+    mail_server.sendmail(email_sender, email_receiver, email_message)
+    mail_server.close()
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Dogsitter
-
+This is a work in progress and is, as of right now, pretty much pseudocode as it has not yet been tested yet at all. Do not clone this repo and expect it to work.
 ## What it is
-This program controls an Arduino, an RPi, 2 relays, and a number of sensors. The arduino acts as a signal processor for the analog sensors, and the RPi handles the system's output.
+This program controls an Arduino, an RPi, 2 relays, and a number of sensors. The Arduino acts as a signal processor for the analog sensors, and the RPi handles the system's output.
 ## What it does
 It tracks whether my dog is upstairs or downstairs, and turns a light on in her location if it's after sunset. It also tracks the noise level in the house, and if there is nearby construction or a garbage truck outside, it plays white noise over the stereo. Finally, it logs the temperature in the house over the day.
-
-This is a work in progress and is, as of right now, pretty much pseudocode as it has not yet been tested yet at all. Do not clone this repo and expect it to work.
+## How it works
+The Arduino reads in data from the temperature sensor and the sound detection module. It calculates the exact temperature and decible level from these values, and prints them to serial. The Arduino code is not yet written.
